145422b894fcb855b05d9b82f3cde3bf9354e570
anonymous anonymous <jelani@firstdraft.com>
2021-11-19 18:15:39 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,16 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :email, :string
+  attribute :password, :string
+  attribute :is_premium, :boolean
+  attribute :profile_picture, :string
+  attribute :bio, :string
+  attribute :username, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
