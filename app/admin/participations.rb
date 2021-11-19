70f46c55397b104f94d8c6d19622bf97579e1382
anonymous anonymous <jelani@firstdraft.com>
2021-11-19 18:14:37 +0000

    Whitelist columns in Participation for ActiveAdmin

--- app/admin/participations.rb
+++ app/admin/participations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Participation do
 
+ permit_params :user_id, :challenge_id, :team_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
