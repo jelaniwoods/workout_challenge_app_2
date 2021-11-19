a2012c4edcafad87daad9cec20eaa942ee98b902
anonymous anonymous <jelani@firstdraft.com>
2021-11-19 18:14:39 +0000

    Whitelist columns in Team for ActiveAdmin

--- app/admin/teams.rb
+++ app/admin/teams.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Team do
 
+ permit_params :team_name, :team_picture, :challenge_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
