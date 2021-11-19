085364e5b776a654eb6c97c15aa92f475af4a4d4
anonymous anonymous <jelani@firstdraft.com>
2021-11-19 18:15:28 +0000

    Only privilege user can modify privilege

--- app/controllers/privileges_controller.rb
+++ app/controllers/privileges_controller.rb
@@ -1,4 +1,6 @@
 class PrivilegesController < ApplicationController
+  before_action :current_user_must_be_privilege_user, only: [:edit, :update, :destroy] 
+
   before_action :set_privilege, only: [:show, :edit, :update, :destroy]
 
   # GET /privileges
@@ -57,6 +59,14 @@ class PrivilegesController < ApplicationController
 
 
   private
+
+  def current_user_must_be_privilege_user
+    set_privilege
+    unless current_user == @privilege.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_privilege
       @privilege = Privilege.find(params[:id])
